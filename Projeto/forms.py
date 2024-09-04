ff63f5ca4bffbc295a8d1b7f61fafeaaf49e20d5
John Chappell <john.claude.cameron@gmail.com>
2024-09-04 11:23:04 -0300

    Comentario

--- Projeto/forms.py
+++ Projeto/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DateField, TextAreaField
 from wtforms.fields.simple import EmailField
 from wtforms.validators import DataRequired, Length, Email
-
+# awo
 class FormularioRegistro(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired(), Length(min=4, max=25)])
     email = EmailField('Email',  validators=[DataRequired(), Email()])
