8f0a37da69b9555f399b4aa26471aee7c7b1b8fb
John Chappell <john.claude.cameron@gmail.com>
2024-09-04 23:16:37 -0300

    Dashboard.html, adicionar a tabela de eventos do usuario

--- Projeto/main.py
+++ Projeto/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import redirect
 
@@ -10,24 +10,34 @@ def index():
     from forms import FormularioLogin
     from werkzeug.security import check_password_hash
 
+    login = None
+
     formsLogin = FormularioLogin()
 
     if formsLogin.validate_on_submit():
         usuarioNome = formsLogin.nome.data
         usuarioDB = Usuario.query.filter_by(nome=usuarioNome).first()
-        print(usuarioDB)
+
+        print(f'>> Nome: {usuarioNome} >> UserDB: {usuarioDB}')
+
+        if usuarioDB is None:
+            print(f'Login: {login}')
+            print('>> Login ou senha incorretos')
+            flash('Login ou senha incorretos')
 
         if usuarioDB:
             senhaDigitada = formsLogin.senha.data
             senhaDB = usuarioDB.senha
 
             if check_password_hash(senhaDB, senhaDigitada):
-                print(f'>> Usuario: {usuarioNome}')
                 login_user(usuarioDB)
+                login = True
                 return redirect('dashboard')
             else:
                 # pop up no site?
+                login = False
                 print('>> Login ou senha incorretos')
+                flash('Login ou senha incorretos')
                 return redirect(url_for('index'))
 
     return render_template('index.html', form=formsLogin)
@@ -60,11 +70,25 @@ def register():
 @app.route('/logout')
 def logout():
     logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    nomeUsuario = current_user.nome
+    return render_template('dashboard.html', usuario=nomeUsuario)
+
+@app.route('/criar_evento', methods=['GET', 'POST'])
+def criar_evento():
+    return render_template('criarEvento.html')
+
+@app.route('/editar_evento/<int:evento_id>', methods=['GET', 'POST'])
+def editar_evento():
+    pass
+
+@app.route('/deletar_evento/<int:evento_id>', methods=['GET', 'POST'])
+def deletar_evento():
+    pass
 
 if __name__ == '__main__':
     app.run(debug = True)
